1a239d750cec030ae69323d2f9be939ac70ae255
TanzidAnan <tanzid.anan125@gmail.com>
2024-12-12 22:51:04 +0600

    data one data

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(express.json())
 app.use(cors())
@@ -35,6 +35,12 @@ async function run() {
         app.get('/jobs', async (req, res) => {
             const cursor = jobsCollection.find();
             const result =await cursor.toArray();
+            res.send(result)
+        })
+        app.get('/jobs/:id',async(req,res) =>{
+            const id =req.params.id;
+            const query ={_id:new ObjectId(id)}
+            const result =await jobsCollection.findOne(query);
             res.send(result)
         })
 

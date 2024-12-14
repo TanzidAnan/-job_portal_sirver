9a2da2bf801ad694167c8fcd77b2d81195aa99a0
TanzidAnan <tanzid.anan125@gmail.com>
2024-12-14 23:40:17 +0600

    job add

--- index.js
+++ index.js
@@ -49,6 +49,16 @@ async function run() {
             const email =req.query.email;
             const query ={applicent_email:email};
             const result =await jobsApplicationCollection.find(query).toArray();
+            for(const application of result){
+                console.log(application.job_id);
+                const query1 ={_id: new ObjectId(application.job_id)};
+                const job =await jobsCollection.findOne(query1);
+                if(job){
+                    application.title =job.title
+                    application.company=job.company
+                    application.company_logo=job.company_logo
+                }
+            }
             res.send(result)
         })
 
